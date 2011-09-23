540e239a29e99a2693e0754798a0afb086f89821
Ras Kam <rkamalov@hotmail.com>
2011-09-23 06:31:38 -0700

    removed temporary folder

--- public/javascripts/ext_init.js
+++
@@ -1,6 +0,0 @@
-Ext.BLANK_IMAGE_URL = '/extjs/resources/images/default/s.gif';
-Ext.ns('Exchange');
-
-function callToRemote(url) {
-  new Ajax.Request(url, {asynchronous:true, evalScripts:true});
-}
